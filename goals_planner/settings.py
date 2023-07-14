dedb9e7fa61d6a6aed31dba43ce44b21c65f39e2
yasmirnov <iaroslav.irnovsm@gmail.com>
2023-07-14 19:21:55 +0300

    port bug fix

--- goals_planner/settings.py
+++ goals_planner/settings.py
@@ -63,7 +63,7 @@ DATABASES = {
         'NAME': env.str('POSTGRES_DB'),
         'USER': env.str('POSTGRES_USER'),
         'PASSWORD': env.str('POSTGRES_PASSWORD'),
-        'HOST': env.str('POSTGRES_HOST', default='127.0.0.1'),
+        'HOST': 'postgres_db',
         'PORT': '5432',
     }
 }
