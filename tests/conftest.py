6a1894afd39f658f86f7f8111399f8983c9b15e8
yasmirnov <iaroslav.irnovsm@gmail.com>
2023-08-09 12:58:00 +0300

    tests added upd

---
+++ tests/conftest.py
@@ -0,0 +1,52 @@
+import pytest
+from django.test import Client
+from pytest_factoryboy import register
+from rest_framework.test import APIClient
+
+from tests.factories import BoardFactory, BoardParticipantFactory, CategoryFactory, GoalFactory, UserFactory
+
+# Factories
+register(UserFactory)
+register(BoardFactory)
+register(BoardParticipantFactory)
+register(CategoryFactory)
+register(GoalFactory)
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def authenticated_user():
+    user = UserFactory.create()
+    password = user.password
+    user.set_password(password)
+    user.save()
+    create_instances_for_user(user)
+    client = Client()
+    client.login(username=user.username, password=password)
+    return {'client': client, 'user': user, 'password': password}
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def users():
+    users = UserFactory.create_batch(2)
+    for user in users:
+        create_instances_for_user(user)
+    return users
+
+
+def create_instances_for_user(user) -> None:
+    board = BoardFactory()
+    BoardParticipantFactory(user=user, board=board, role=1)
+    categories = CategoryFactory.create_batch(2, user=user, board=board)
+    for cat in categories:
+        GoalFactory.create(user=user, category=cat)
+
+
+@pytest.fixture()
+def auth_client(client, user) -> APIClient:
+    client.force_login(user)
+    return client
+
+def another_user(user_factory):
+    return user_factory.create()
---
+++ tests/factories.py
@@ -0,0 +1,58 @@
+import factory
+from factory import Faker
+from factory.django import DjangoModelFactory
+
+from core.models import User
+from goals.models import Board, BoardParticipant, Goal, GoalCategory
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = Faker('user_name')
+    first_name = Faker('first_name')
+    last_name = Faker('last_name')
+    email = Faker('email')
+    password = Faker('password')
+
+
+class BoardFactory(DjangoModelFactory):
+    class Meta:
+        model = Board
+
+    title = Faker('word')
+    is_deleted = False
+
+
+class BoardParticipantFactory(DjangoModelFactory):
+    class Meta:
+        model = BoardParticipant
+
+    user = None
+    board = None
+    role = 1
+
+
+class CategoryFactory(DjangoModelFactory):
+    class Meta:
+        model = GoalCategory
+
+    title = Faker('word')
+    is_deleted = False
+    user = None
+    board = None
+
+
+class GoalFactory(DjangoModelFactory):
+    class Meta:
+        model = Goal
+
+    title = Faker('word')
+    user = None
+    category = None
+
+
+class LoginRequest(factory.DictFactory):
+    username = factory.Faker('user_name')
+    password = factory.Faker('password')
